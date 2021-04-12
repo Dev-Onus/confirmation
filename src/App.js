b7664c4918d70f9d119c808ad975152b61eb8e98
renjini <lrenjinik@gmail.com>
2021-04-12 18:53:50 +0530

    refactor: 💡 updated router component name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import ConfirmationRouter from './components/confirmationRouter';
-import Toolbar from './components/Header/Toolbar';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import './App.css';
+import RouterData from "./components/Router";
+import Toolbar from "./components/Header/Toolbar";
+import { Layout, Menu, Breadcrumb } from "antd";
+import "./App.scss";
 
 const { Header, Content, Footer } = Layout;
 function App() {
   return (
     <>
-    
-    <Layout>
-       <Header> <Toolbar /> </Header>
+      <RouterData />
+      {/* <Layout>
+        <Header>
+          {" "}
+          <Toolbar />{" "}
+        </Header>
         <Content>
-        Text sample
-        <ConfirmationRouter />
+          Text sample
+          <Router />
         </Content>
         <Footer>Footer</Footer>
-      </Layout>
-</>
+      </Layout> */}
+    </>
   );
 }
 
