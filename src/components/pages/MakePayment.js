f637967c4daf7a576eebb7ea1f184163f45b2ee4
renjini <lrenjinik@gmail.com>
2021-04-15 10:09:40 +0530

    refactor: 💡 add makepayment page

---
+++ src/components/pages/MakePayment.js
@@ -0,0 +1,59 @@
+import { Typography } from "antd";
+
+const { Paragraph, Title, Text } = Typography;
+
+const MakePaymentComponentComponent = () => {
+  return (
+    <>
+      <div className={false} style={{}}>
+        <h1 className={false} style={{}}>
+          Make Payement
+        </h1>
+        <div className={false} style={{}}>
+          <Typography>
+            <Paragraph
+              code={false}
+              copyable={false}
+              delete={false}
+              disabled={false}
+              editable={false}
+              ellipsis={false}
+              mark={false}
+              strong={false}
+              underline={false}
+            >
+              <Title
+                code={false}
+                copyable={false}
+                delete={false}
+                disabled={false}
+                editable={false}
+                ellipsis={false}
+                level={5}
+                mark={false}
+                underline={false}
+              >
+                Test Biller
+              </Title>
+              <Text
+                code={false}
+                copyable={false}
+                delete={false}
+                disabled={false}
+                editable={false}
+                ellipsis={false}
+                keyboard={false}
+                mark={false}
+                strong={false}
+                underline={false}
+              >
+                Eitem 3
+              </Text>
+            </Paragraph>
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+};
+export default MakePaymentComponentComponent;
